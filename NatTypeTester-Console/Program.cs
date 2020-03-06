d6eec7db91d190e16db32c197b2d3b012f667201
Amazing_DM <amazingdmdd@gmail.com>
2020-03-06 11:59:23 +0800

    Netch NTT

--- NatTypeTester-Console/Program.cs
+++ NatTypeTester-Console/Program.cs
@@ -3,23 +3,23 @@ using System;
 
 namespace NatTypeTester_Console
 {
-	internal static class Program
-	{
-		private static void Main(string[] args)
-		{
-			var server = @"stun.qq.com";
-			ushort port = 3478;
-			if (args.Length > 0)
-			{
-				server = args[0];
-			}
-			if (args.Length > 1)
-			{
-				ushort.TryParse(args[1], out port);
-			}
-			var res = NetUtils.NatTypeTestCore(NetUtils.DefaultLocalEnd, server, port);
-			var natType = res.Item1;
-			Console.WriteLine(string.IsNullOrWhiteSpace(natType) ? @"Error" : natType);
-		}
-	}
+    internal static class Program
+    {
+        private static void Main(string[] args)
+        {
+            var server = @"stun.qq.com";
+            ushort port = 3478;
+            if (args.Length > 0)
+            {
+                server = args[0];
+            }
+            if (args.Length > 1)
+            {
+                ushort.TryParse(args[1], out port);
+            }
+            var res = NetUtils.NatTypeTestCore(NetUtils.DefaultLocalEnd, server, port);
+            var natType = res.Item1 + "#" + res.Item2 + "#" + res.Item3;
+            Console.WriteLine(string.IsNullOrWhiteSpace(natType) ? @"Error" : natType);
+        }
+    }
 }
